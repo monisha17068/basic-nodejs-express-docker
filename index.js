3d4953557f0527bac7326ca97ddb810a8944b182
chitu1629 <108867392+chitu1629@users.noreply.github.com>
2024-02-02 11:05:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 const port = Environment.port || 3000;
 
 server.get('/', (request, response) => {
-    response.send('Hello World!');
+    response.send('Hello Team! Deployment for node.js application is completed ');
 });
   
 server.listen(port, () => {
